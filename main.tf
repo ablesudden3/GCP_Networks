a10ee416a9bebedabb9022cf42d3d162ebf454d0
ablesudden3 <64771032+ablesudden3@users.noreply.github.com>
2020-12-23 13:01:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,10 +17,10 @@ resource "google_compute_subnetwork" "modular_tf_subnet" {
   region        = element(data.google_compute_regions.available.names, count.index)
   network       = var.VPC_Name
  
-  #secondary_ip_range {
-   # range_name    = "tf-test-secondary-range-update${count.index}"
-    #ip_cidr_range = "192.168.${count.index}.0/24"
-   #}
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update${count.index}"
+    ip_cidr_range = "192.168.${count.index}.0/24"
+   }
 
   depends_on = [google_compute_network.vpc_network]
 
